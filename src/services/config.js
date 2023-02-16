ad209399ee00d21c069be592e74773cd89a7571e
MyMonsters <62538105+MyMonsters@users.noreply.github.com>
2023-02-16 13:54:06 +0800

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-const devBaseURL = 'http://101.34.205.91:7878';
-const proBaseURL = 'http://101.34.205.91:7878';
+const devBaseURL = 'http://101.34.205.91:3000';
+const proBaseURL = 'http://101.34.205.91:3000';
 export const BASE_URL =
   process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL;
 export const TIME_OUT = 5000;
